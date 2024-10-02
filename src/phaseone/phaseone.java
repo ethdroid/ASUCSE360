9fcee0f5b32661dd8974152ad49627097f36055a
Luka Petrovic <lpetrov1@asu.edu>
2024-10-01 18:16:08 -0700

    new

--- src/phaseone/phaseone.java
+++ src/phaseone/phaseone.java
@@ -1,6 +1,7 @@
 public class phaseone {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("hey");
+        System.out.println("hii");
+        System.out.println("hii");
     }
 }
