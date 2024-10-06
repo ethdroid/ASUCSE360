e8dd5579d672360730a828e1e8ce736e0cfef86e
ethdroid <ethdroid@gmail.com>
2024-10-05 19:22:51 -0700

    new beginnings pt 2

--- src/phaseone/UserLogin.java
+++ src/phaseone/UserLogin.java
@@ -1,13 +1,3 @@
-class PasswordEvaluator {
-    public static String evaluatePassword(String password) {
-        // Check if password is at least 6 characters
-        if (password.length() < 6) {
-            return "Password must be at least 6 characters long.";
-        }
-        return ""; // Return empty string if password is valid
-    }
-}
-
 public class UserLogin {
 
     public static void main(String[] args) {
