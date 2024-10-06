cdb3b34ddec176bac8a20a7ad5d611ab5b6ecba2
ethdroid <ethdroid@gmail.com>
2024-10-05 19:34:18 -0700

    I have committed

--- src/phaseone/userclass.java
+++ src/phaseone/userclass.java
@@ -2,11 +2,22 @@ public class userclass {
     private String username;
     private String password;
     private String role;
+    private String email;
+    private String firstname;
+    private String middlename;
+    private String lastname;
+    private String preferredName;
 
-    public userclass(String username, String password, String role) {
+
+    public userclass(String username, String password, String role, String email, String firstname, String middlename, String lastname, String preferredName) {
         this.username = username;
         this.password = password;
         this.role = role;
+        this.email = email;
+        this.firstname = firstname;
+        this.middlename = middlename;
+        this.lastname = lastname;
+        this.preferredName = preferredName;
     }
 
     public String getUsername() {
@@ -33,11 +44,54 @@ public class userclass {
         this.role = role;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getMiddlename() {
+        return middlename;
+    }
+
+    public void setMiddlename(String middlename) {
+        this.middlename = middlename;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getPreferredName() {
+        return preferredName;
+    }
+
+    public void setPreferredName(String preferredName) {
+        this.preferredName = preferredName;
+    }
+
     public String toString() {
-        return "User{" +
-            "username='" + username + '\'' +
-            ", password='[PROTECTED]" +
-            ", role='" + role + '\'' +
-            '}';
+        return "Username: " + username + " , " +
+                "Password: " + password + " , " +
+                "Role: " + role + ", " +
+                "Email: " + email + " , " +
+                "Firstname: " + firstname + " , " +
+                "Middlename: " + middlename + " , " +
+                "Lastname: " + lastname + " , " +
+                "Preferred Name: " + preferredName;
     }
 }
